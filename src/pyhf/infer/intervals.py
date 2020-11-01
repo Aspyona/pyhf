fd8bb90bac9d01b4c600c83ba47c1119ab096109
Paul Feichtinger <paul.feichtinger@oedoc.at>
2020-11-01 13:37:34 +0100

    modified upperlimit function to also work with pytorch indexing, pass arguments to hypotest

--- src/pyhf/infer/intervals.py
+++ src/pyhf/infer/intervals.py
@@ -6,10 +6,10 @@ import numpy as np
 
 def _interp(x, xp, fp):
     tb, _ = get_backend()
-    return tb.astensor(np.interp(x, xp.tolist(), fp.tolist()))
+    return tb.astensor(np.interp(x, xp, fp))
 
 
-def upperlimit(data, model, scan, level=0.05, return_results=False):
+def upperlimit(data, model, scan, level=0.05, return_results=False, **kwargs):
     """
     Calculate an upper limit interval ``(0, poi_up)`` for a single
     Parameter of Interest (POI) using a fixed scan through POI-space.
@@ -50,14 +50,14 @@ def upperlimit(data, model, scan, level=0.05, return_results=False):
     """
     tb, _ = get_backend()
     results = [
-        hypotest(mu, data, model, qtilde=True, return_expected_set=True) for mu in scan
+        hypotest(mu, data, model, qtilde=True, return_expected_set=True, **kwargs,) for mu in scan
     ]
     obs = tb.astensor([[r[0]] for r in results])
     exp = tb.astensor([[r[1][idx] for idx in range(5)] for r in results])
     result_arrary = tb.concatenate([obs, exp], axis=1).T
 
     # observed limit and the (0, +-1, +-2)sigma expected limits
-    limits = [_interp(level, result_arrary[idx][::-1], scan[::-1]) for idx in range(6)]
+    limits = [_interp(level, result_arrary[idx].tolist()[::-1], scan.tolist()[::-1]) for idx in range(6)]
     obs_limit, exp_limits = limits[0], limits[1:]
 
     if return_results:
